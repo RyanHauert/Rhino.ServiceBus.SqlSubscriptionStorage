ee2ed5e9d0b8ea83838a26d82a36f661ec59b360
Corey Kaylor <corey@kaylors.net>
2012-08-07 10:29:14 -0600

    Initial commit

---
+++ Rhino.ServiceBus.SqlSubscriptionStorage/SqlSubscriptionStorage.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Rhino.ServiceBus.Impl;
+using Rhino.ServiceBus.Internal;
+using Rhino.ServiceBus.MessageModules;
+using Rhino.ServiceBus.Messages;
+using log4net;
+
+namespace Rhino.ServiceBus
+{
+    public class SqlSubscriptionStorage : ISubscriptionStorage, IMessageModule
+    {
+        private readonly string _connectionString;
+        private readonly ILog _logger = LogManager.GetLogger(typeof(SqlSubscriptionStorage));
+
+        public SqlSubscriptionStorage()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["Rhino.ServiceBus.SqlSubscriptionStorage"].ConnectionString;
+        }
+
+        public void AddLocalInstanceSubscription(IMessageConsumer consumer)
+        {
+        }
+
+        public bool AddSubscription(string type, string endpoint)
+        {
+            Subscription((subscription =>
+            {
+                subscription.AddSubscription(type, endpoint);
+                subscription.Commit();
+            }));
+            RaiseSubscriptionChanged();
+            _logger.DebugFormat("Added subscription for {0} message at endpoint {1}", type, endpoint);
+            return true;
+        }
+
+        public bool ConsumeAddInstanceSubscription(AddInstanceSubscription subscription)
+        {
+            return true;
+        }
+
+        public bool ConsumeAddSubscription(AddSubscription subscription)
+        {
+            return AddSubscription(subscription.Type, subscription.Endpoint.Uri.AbsoluteUri);
+        }
+
+        public bool ConsumeRemoveInstanceSubscription(RemoveInstanceSubscription subscription)
+        {
+            return true;
+        }
+
+        public bool ConsumeRemoveSubscription(RemoveSubscription subscription)
+        {
+            RemoveSubscription(subscription.Type, subscription.Endpoint.Uri.AbsoluteUri);
+            return true;
+        }
+
+        public object[] GetInstanceSubscriptions(Type type)
+        {
+            return new object[0];
+        }
+
+        public IEnumerable<Uri> GetSubscriptionsFor(Type type)
+        {
+            IEnumerable<Uri> subscriptions = new List<Uri>();
+            try
+            {
+                Subscription(subscription =>
+                {
+                    subscriptions = subscription.GetSubscriptions(type.FullName).ToList();
+                    subscription.Commit();
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(string.Format("Error getting subscriptions for {0}", type.FullName), ex);
+            }
+            return subscriptions;
+        }
+
+        public void Init(ITransport transport, IServiceBus bus)
+        {
+            transport.AdministrativeMessageArrived += TransportAdministrativeMessageArrived;
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public void RemoveLocalInstanceSubscription(IMessageConsumer consumer)
+        {
+        }
+
+        public void RemoveSubscription(string type, string endpoint)
+        {
+            Subscription(subscription =>
+            {
+                subscription.RemoveSubscription(type, endpoint);
+                subscription.Commit();
+            });
+            _logger.DebugFormat("Removed subscription for {0} message at endpoint {1}", type, endpoint);
+        }
+
+        public void Stop(ITransport transport, IServiceBus bus)
+        {
+            transport.AdministrativeMessageArrived -= TransportAdministrativeMessageArrived;
+        }
+
+        void RaiseSubscriptionChanged()
+        {
+            var action = SubscriptionChanged;
+            if (action == null)
+                return;
+            action();
+        }
+
+        void Subscription(Action<SubscriptionStorageActions> action)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction(IsolationLevel.ReadUncommitted))
+                {
+                    var subscriptionStorageActions = new SubscriptionStorageActions(connection, transaction);
+                    action(subscriptionStorageActions);
+                }
+            }
+        }
+
+        bool TransportAdministrativeMessageArrived(CurrentMessageInformation msgInfo)
+        {
+            var subscription = msgInfo.Message as AddSubscription;
+            if (subscription != null)
+                return ConsumeAddSubscription(subscription);
+            var removeSubscription = msgInfo.Message as RemoveSubscription;
+            if (removeSubscription != null)
+                return ConsumeRemoveSubscription(removeSubscription);
+            var addInstanceSubscription = msgInfo.Message as AddInstanceSubscription;
+            if (addInstanceSubscription != null)
+                return ConsumeAddInstanceSubscription(addInstanceSubscription);
+            var removeInstanceSubscription = msgInfo.Message as RemoveInstanceSubscription;
+            if (removeInstanceSubscription != null)
+                return ConsumeRemoveInstanceSubscription(removeInstanceSubscription);
+            return false;
+        }
+
+        public event Action SubscriptionChanged;
+    }
+}
---
+++ Rhino.ServiceBus.SqlSubscriptionStorage/SubscriptionStorageActions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Rhino.ServiceBus
+{
+    internal class SubscriptionStorageActions
+    {
+        private readonly SqlConnection connection;
+        private readonly SqlTransaction transaction;
+
+        internal SubscriptionStorageActions(SqlConnection connection, SqlTransaction transaction)
+        {
+            this.connection = connection;
+            this.transaction = transaction;
+        }
+
+        public bool AddSubscription(string type, string subscriberUri)
+        {
+            var sqlCommand = new SqlCommand("[RSB].[AddSubscription]", connection, transaction);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@messageTypeName", type);
+            sqlCommand.Parameters.AddWithValue("@subscriberUri", subscriberUri);
+            return Convert.ToBoolean(sqlCommand.ExecuteScalar());
+        }
+
+        public void Commit()
+        {
+            transaction.Commit();
+        }
+
+        public IEnumerable<Uri> GetSubscriptions(string type)
+        {
+            var sqlCommand = new SqlCommand("[RSB].[GetAllSubscriptionsForType]", connection, transaction)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            sqlCommand.Parameters.AddWithValue("@messageTypeName", type);
+            using (var sqlDataReader = sqlCommand.ExecuteReader(CommandBehavior.SingleResult))
+            {
+                while (sqlDataReader.Read())
+                    yield return new Uri(sqlDataReader.GetString(0));
+            }
+        }
+
+        public void RemoveSubscription(string type, string subscriberUri)
+        {
+            var sqlCommand = new SqlCommand("[RSB].[RemoveSubscription]", connection, transaction);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@messageTypeName", type);
+            sqlCommand.Parameters.AddWithValue("@subscriberUri", subscriberUri);
+            sqlCommand.ExecuteNonQuery();
+        }
+    }
+}
